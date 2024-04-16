16c08b002bab2897e3ba79ed32472ab628ac6134
Attila Botz <attila.botz@barion.com>
2024-04-16 13:25:43 +0200

    Redpanda monolith consumer and producer with Confluent.Kafka library

---
+++ Monolith/ConsoleConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+
+namespace ConsoleConsumer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "console-consumer",
+                BootstrapServers = "localhost:19092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            
+            using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+            {
+                consumer.Subscribe("current-time");
+                
+                while (true)
+                {
+                    var consumeResult = consumer.Consume();
+                    System.Console.WriteLine($"Consumed message '{consumeResult.Message.Value}' at: '{consumeResult.TopicPartitionOffset}'");
+                }
+            }
+        }
+    }
+}
---
+++ Monolith/ConsoleProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace ConsoleProducer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:19092"
+            };
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                while (true)
+                {
+                    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+
+                    producer.Produce("current-time",
+                        new Message<string, string> { Key = "CurrentTime", Value = DateTime.Now.ToString() });
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+    }
+}
