0aceef5c5f8c6d39148f2e68d98531699417e2e2
Attila Botz <attila.botz@barion.com>
2024-04-16 14:02:35 +0200

    Migration

---
+++ Miroservices/Consumer/MicroService.Consumer.DataAccess/Migrations/20240416120057_NoteId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroService.Consumer.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoteId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NoteId",
+                table: "Note",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NoteId",
+                table: "Note");
+        }
+    }
+}
