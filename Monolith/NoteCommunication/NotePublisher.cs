1bc4d5bd0575606a6d2b303ca1d443484a6b977a
Attila Botz <attila.botz@barion.com>
2024-05-02 14:52:40 +0200

    Fixing packages

--- Monolith/NoteCommunication/NotePublisher.cs
+++ Monolith/NoteCommunication/NotePublisher.cs
@@ -4,6 +4,10 @@ namespace NoteCommunication
 {
     public class NotePublisher : INotePublisher
     {
+        public NotePublisher()
+        {
+            
+        }
         public void Publish(NoteModel note)
         {
             return;
